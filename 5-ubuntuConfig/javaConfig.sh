164f1dfd6a7a50210a3c58c5f6fe6586465e76a8
lvbing <lvyanbing9@gmail.com>
2019-10-19 01:27:44 +0800

    update javaConfig.sh

--- 5-ubuntuConfig/javaConfig.sh
+++ 5-ubuntuConfig/javaConfig.sh
@@ -5,48 +5,66 @@ PROMPT='>>>>>>>>>>>'
 
 PATH_TAR_JDK=/home/lvbing
 FILE_NAME_JDK=jdk-8u181-linux-x64.tar.gz
-FILE_CONFIG=/etc/profile
+FILE_USER_BASHRC=/home/lvbing/.bashrc
+FILE_ROOT_BASHRC=/root/.bashrc
 
+echo "$PROMPT back file:$FILE_USER_BASHRC --> $FILE_USER_BASHRC.back"
+sudo cp $FILE_USER_BASHRC $FILE_USER_BASHRC.back
 
-PATCH_APP_INSTALL=/usr/local/src/${FILE_NAME_JDK%%.*}
-echo "$PROMPT set install dir:$PATCH_APP_INSTALL"
-sudo rm -r $PATCH_APP_INSTALL
-sudo mkdir $PATCH_APP_INSTALL
+echo "$PROMPT back file:$FILE_ROOT_BASHRC --> $FILE_ROOT_BASHRC.back"
+sudo cp $FILE_ROOT_BASHRC $FILE_ROOT_BASHRC.back
 
-echo "$PROMPT Decompression flle <$FILE_NAME_JDK> to <$PATCH_APP_INSTALL>."
-sudo tar -zxvf $PATH_TAR_JDK/$FILE_NAME_JDK  -C $PATCH_APP_INSTALL
+PATH_APP_INSTALL=/usr/local/src/${FILE_NAME_JDK%%.*}
+echo "$PROMPT rm dir:$PATH_APP_INSTALL"
+sudo rm -r $PATH_APP_INSTALL
+echo "$PROMPT set install dir:$PATH_APP_INSTALL"
+sudo mkdir $PATH_APP_INSTALL
 
-PATCH_APP_INSTALL=$PATCH_APP_INSTALL/$(ls -l $PATCH_APP_INSTALL |awk '/^d/ {print $NF}')
-echo "$PROMPT change install dir:$PATCH_APP_INSTALL"
+echo "$PROMPT Decompression flle <$FILE_NAME_JDK> to <$PATH_APP_INSTALL>."
+sudo tar -zxvf $PATH_TAR_JDK/$FILE_NAME_JDK  -C $PATH_APP_INSTALL
 
-STRING_CONFIG="JAVA_HOME=$PATCH_APP_INSTALL\nCLASSPATH=\$JAVA_HOME/lib/\nPATH=\$PATH:\$JAVA_HOME/bin\nexport JAVA_HOME PATH CLASSPATH\n"
+PATH_APP_INSTALL=$PATH_APP_INSTALL/$(ls -l $PATH_APP_INSTALL |awk '/^d/ {print $NF}')
+echo "$PROMPT change install dir:$PATH_APP_INSTALL"
+
+STRING_CONFIG="\nJAVA_HOME=$PATH_APP_INSTALL\nCLASSPATH=\$JAVA_HOME/lib/\nPATH=\$PATH:\$JAVA_HOME/bin\nexport JAVA_HOME PATH CLASSPATH\n"
 echo "$PROMPT get config string:"
 echo $STRING_CONFIG
 
-echo "$PROMPT get config string referenc:"
-STRING_REFERENCE="JAVA_HOME=$PATCH_APP_INSTALL"
+echo "$PROMPT get referenc config string:"
+STRING_REFERENCE="JAVA_HOME=$PATH_APP_INSTALL"
 echo $STRING_REFERENCE
 
 LOG_OUT=
-PARAM=$(cat $FILE_CONFIG | grep "$STRING_REFERENCE")
-echo "$PROMPT get grep results:"
-echo $PARAM
 
+TEMP_STRING_REFERENCE=$STRING_REFERENCE
+TEMP_STRING_CONFIG=$STRING_CONFIG
+TEMP_FILE_CONFIG=$FILE_USER_BASHRC
+PARAM=$(cat $TEMP_FILE_CONFIG| grep "$TEMP_STRING_REFERENCE")
 if [ "$PARAM" != "" ]; then
   echo "----------------------$FILE_CONFIG----------------------------"
-  cat $FILE_CONFIG
-  echo "----------------------$FILE_CONFIG----------------------------"
-  LOG_OUT=${LOG_OUT}"\n$PROMPT already config <$FILE_CONFIG> !"
+  cat $TEMP_FILE_CONFIG
+  LOG_OUT=${LOG_OUT}"\n$PROMPT already config <$TEMP_FILE_CONFIG> !"
 else
-  echo $STRING_CONFIG >> $FILE_CONFIG
+  echo $TEMP_STRING_CONFIG >> $TEMP_FILE_CONFIG
   echo "----------------------$FILE_CONFIG----------------------------"
-  cat $FILE_CONFIG
+  cat $TEMP_FILE_CONFIG
+  LOG_OUT=${LOG_OUT}"\n$PROMPT fist config,please < source $TEMP_FILE_CONFIG >!"
+fi
+
+TEMP_STRING_REFERENCE=$STRING_REFERENCE
+TEMP_STRING_CONFIG=$STRING_CONFIG
+TEMP_FILE_CONFIG=$FILE_ROOT_BASHRC
+PARAM=$(cat $TEMP_FILE_CONFIG| grep "$TEMP_STRING_REFERENCE")
+if [ "$PARAM" != "" ]; then
   echo "----------------------$FILE_CONFIG----------------------------"
-  LOG_OUT=${LOG_OUT}"\n$PROMPT fist config,please :source $FILE_CONFIG"
+  cat $TEMP_FILE_CONFIG
+  LOG_OUT=${LOG_OUT}"\n$PROMPT already config <$TEMP_FILE_CONFIG> !"
+else
+  echo $TEMP_STRING_CONFIG >> $TEMP_FILE_CONFIG
+  echo "----------------------$FILE_CONFIG----------------------------"
+  cat $TEMP_FILE_CONFIG
+  LOG_OUT=${LOG_OUT}"\n$PROMPT fist config,please < source $TEMP_FILE_CONFIG >!"
 fi
 
 echo $LOG_OUT
-
-
-
 
